d0a51788d0b9ae6ef9e2f0fcd088582ed5b21996
Ash Wilson <ash.d.wilson@gmail.com>
2019-01-21 21:00:59 -0800

    chg: usr: Scripts automatically install with Python package.
    Added generator function yield_rows() to all abstractions.

--- Dockerfile
+++ Dockerfile
@@ -10,3 +10,26 @@ WORKDIR /src/
 RUN pip install -e .
 
 RUN py.test --cov=kismetdb --cov-report=term-missing
+
+# Install simplekml for tests.
+RUN pip install simplekml
+
+# Test KML script
+RUN kismet_log_to_kml --in=./tests/assets/testdata.kismet --out=./out.kml
+
+# Test CSV script
+RUN kismet_log_to_csv --in=./tests/assets/testdata.kismet --table=devices --out=./devices.csv
+
+RUN kismet_log_to_csv --in=./tests/assets/testdata.kismet --table=packets --out=packets.csv
+
+RUN kismet_log_to_csv --in=./tests/assets/testdata.kismet --table=datasources --out=./datasources.csv
+
+RUN  kismet_log_to_csv --in=./tests/assets/testdata.kismet --table=alerts --out=./alerts.csv
+
+# Test pcap script
+RUN kismet_log_to_pcap --in=./tests/assets/testdata.kismet --out=./out.pcap
+
+# Test devices to json script
+RUN which kismet_log_devices_to_json
+
+RUN kismet_log_devices_to_json --in=./tests/assets/testdata.kismet --out=./out.json
---
+++ kismetdb/scripts/log_devices_to_json.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+# Simple dumper to extract kismet records and export them as a json
+# array
+
+import argparse
+import datetime
+import json
+import os
+import struct
+import sqlite3
+import sys
+
+import kismetdb
+
+def main():
+    parser = argparse.ArgumentParser(description="Kismet to Pcap Log Converter")
+    parser.add_argument("--in", action="store", dest="infile", help='Input (.kismet) file')
+    parser.add_argument("--out", action="store", dest="outfile", help='Output filename (optional)')
+    parser.add_argument("--start-time", action="store", dest="starttime", help='Only list devices seen after given time')
+    parser.add_argument("--min-signal", action="store", dest="minsignal", help='Only list devices with a best signal higher than min-signal')
+
+    results = parser.parse_args()
+    query_args = {}
+    log_to_single = True
+
+    if results.infile is None:
+        print("Expected --in [file]")
+        sys.exit(1)
+
+    if not os.path.isfile(results.infile):
+        print("Could not find input file '{}'".format(results.infile))
+        sys.exit(1)
+
+    if results.starttime:
+        query_args["first_time_gt"] = results.starttime
+
+    if results.minsignal:
+        query_args["strongest_signal_gt"] = results.minsignal
+    logf = None
+
+    devices_abstraction = kismetdb.Devices(results.infile)
+
+    devs = [row["device"] for row in devices_abstraction.get_all(**query_args)]
+
+    if results.outfile:
+        logf = open(results.outfile, "w")
+        logf.write(json.dumps(devs, sort_keys=True, indent=4,
+                              separators=(',', ': ')))
+    else:
+        print(json.dumps(devs, sort_keys=True, indent=4,
+                         separators=(',', ': ')))
+
+
+if __name__ == "__main__":
+    main()
