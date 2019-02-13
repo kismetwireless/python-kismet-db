d8f7b34f528fb9eb30272ba3d262695d52068f07
Ash Wilson <ash.d.wilson@gmail.com>
2019-02-12 23:08:57 -0800

    new: usr: Support v4 as well as v5 Kismet databases.
    Closes #19

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import os
 import re
 from setuptools import setup
 
+
 def read(file_name):
     with open(os.path.join(os.path.dirname(__file__), file_name), 'r') as f:
         filestring = f.read()
     return filestring
+
 
 def get_version():
     raw_init_file = read("kismetdb/__init__.py")
@@ -13,27 +15,29 @@ def get_version():
     ver = rx_compiled.search(raw_init_file).group(1)
     return ver
 
+
 def build_long_desc():
     return "\n".join([read(f) for f in ["README.rst", "CHANGELOG.rst"]])
 
-setup(name = "kismetdb",
-      version = get_version(),
-      author = "Ash Wilson",
-      author_email = "ash.d.wilson@gmail.com",
-      description = "A python wrapper for the Kismet database",
-      license = "BSD",
-      keywords = "kismet",
-      url = "https://github.com/ashmastaflash/kismet-db",
-      packages = ["kismetdb", "kismetdb.scripts"],
-      install_requires = ["python-dateutil", "simplekml"],
-      long_description = build_long_desc(),
+
+setup(name="kismetdb",
+      version=get_version(),
+      author="Ash Wilson",
+      author_email="ash.d.wilson@gmail.com",
+      description="A python wrapper for the Kismet database",
+      license="BSD",
+      keywords="kismet",
+      url="https://github.com/ashmastaflash/kismet-db",
+      packages=["kismetdb", "kismetdb.scripts"],
+      install_requires=["python-dateutil", "simplekml"],
+      long_description=build_long_desc(),
       entry_points={
           "console_scripts": [
-              "kismet_log_devices_to_json = kismetdb.scripts.log_devices_to_json:main",
+              "kismet_log_devices_to_json = kismetdb.scripts.log_devices_to_json:main",  # NOQA
               "kismet_log_to_csv = kismetdb.scripts.log_to_csv:main",
               "kismet_log_to_kml = kismetdb.scripts.log_to_kml:main",
               "kismet_log_to_pcap = kismetdb.scripts.log_to_pcap:main",
-              "kismet_log_devices_to_filebeat_json = kismetdv.scripts.log_devices_to_filebeat_json:main]},
+              "kismet_log_devices_to_filebeat_json = kismetdb.scripts.log_devices_to_filebeat_json:main"]},  # NOQA
       classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
