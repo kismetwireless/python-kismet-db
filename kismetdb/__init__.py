a74df04474c5a91734878e046196605a67a4c822
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2019-05-23 11:21:47 -0400

    Enable classes Bump version Add integer version

--- kismetdb/__init__.py
+++ kismetdb/__init__.py
@@ -2,7 +2,11 @@ from .alerts import Alerts  # NOQA
 from .base_interface import BaseInterface  # NOQA
 from .data_sources import DataSources  # NOQA
 from .devices import Devices  # NOQA
+from .kismet import Kismet  # NOQA
 from .packets import Packets  # NOQA
+from .snapshots import Snapshots  # NOQA
 from .utility import Utility  # NOQA
 
-__version__ = "2019.05.01"
+__version__ = "2019.05.02"
+__version_int__ = "201900502"
+
