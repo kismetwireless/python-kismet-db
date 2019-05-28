c636845a02d02abbeeab4f6ea7fbb64565d519bd
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2019-05-28 10:43:59 -0400

    Bump version marker Fix license type in __init__

--- kismetdb/__init__.py
+++ kismetdb/__init__.py
@@ -7,9 +7,10 @@ from .packets import Packets  # NOQA
 from .snapshots import Snapshots  # NOQA
 from .utility import Utility  # NOQA
 
-__version__ = "2019.05.02"
+__version__ = "2019.05.03"
 __author__ = "Mike Kershaw, Ash Wilson"
 __copyright__ = "2019, Mike Kershaw"
-__license__ = "BSD"
+__license__ = "GPL2"
 __email__ = "dragorn@kismetwireless.net"
-__version_int__ = "201900502"
+__version_int__ = "201900503"
+
