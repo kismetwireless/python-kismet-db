9affd4539aa054fba025c8ccfc0656e558aeaefd
Ash <ash.d.wilson@gmail.com>
2019-06-22 21:35:06 -0700

    chg: dev: Correcting license to GPL2 in __init__.py. !minor

--- kismetdb/__init__.py
+++ kismetdb/__init__.py
@@ -10,6 +10,6 @@ from .utility import Utility  # NOQA
 __version__ = "2019.05.02"
 __author__ = "Mike Kershaw, Ash Wilson"
 __copyright__ = "2019, Mike Kershaw"
-__license__ = "BSD"
+__license__ = "GPL2"
 __email__ = "dragorn@kismetwireless.net"
 __version_int__ = "201900502"
